37628097c5aaa6e393ebdc9d18c9832f7f782433
YeHtutSoe <yeandzarforever@gmail.com>
2025-01-15 03:55:59 +0630

    update purchase

---
+++ Models/ViewModels/PurchaseItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudPOS.Models.ViewModels
+{
+    public class PurchaseItemViewModel
+    {
+        public string Id { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
