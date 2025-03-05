19f2a9afd9de63db0a5853d08952136aab0a121c
YeHtutSoe <yeandzarforever@gmail.com>
2025-03-05 20:19:16 +0630

    update Price Entity

--- Models/ViewModels/ProductViewModel.cs
+++ Models/ViewModels/ProductViewModel.cs
@@ -5,22 +5,16 @@ namespace CloudPOS.Models.ViewModels
     public class ProductViewModel
     {
         public string Id { get; set; }
-        [Required(ErrorMessage ="Requried Type")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Requried Serial Number")]
         public string Code { get; set; }
         public string? Description { get; set; }
-        [Range(0,double.MaxValue,ErrorMessage = "Sale Price must be positive number")]
         public Decimal SalePrice { get; set; }
         public Decimal DiscountPrice { get; set; }
-        [Range(0, double.MaxValue, ErrorMessage = "Cost Price must be positive number")]
         public int Quantity { get; set; }
         public Decimal CostPrice { get; set; }
-        [Required(ErrorMessage = "Phone Model is required")]
         public string BrandId { get; set; }
         public string BrandInfo { get; set; }
         public IList<BrandViewModel> BrandViewModels { get; set; } //List for UI Binding
-        [Required(ErrorMessage = "Category is required")]
         public string CategoryId { get; set; }
         public string CategoryInfo { get; set; }
         public IList<CategoryViewModel> CategoryViewModels { get; set; } // List for UI Binding
